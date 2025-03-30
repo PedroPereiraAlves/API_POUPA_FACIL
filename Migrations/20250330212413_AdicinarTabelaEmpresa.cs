cdf395c1216b99ab3445a4c2e2c59c6f407047ca
Pedro <pedroalves@dpc.com.br>
2025-03-30 18:25:53 -0300

    Criando uma nova migration para ajustar a aplicação no render

---
+++ Migrations/20250330212413_AdicinarTabelaEmpresa.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace APIPOUPAFACIL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdicinarTabelaEmpresa : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "empresa",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    cnpj = table.Column<string>(type: "text", nullable: true),
+                    nome = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_empresa", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "empresa");
+        }
+    }
+}
